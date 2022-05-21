48859f0a0e2a120418aa2b516aa4804428f88aef
JUVINAO PÉREZ David Jesús <davidjesus.juvinaoperez@amaris.com>
2022-05-20 22:17:35 -0500

    ddddddddd

--- front-admin/components/Layout/index.jsx
+++ front-admin/components/Layout/index.jsx
@@ -14,7 +14,7 @@ import { LateralModal } from 'container/dashboard/styled'
 import { BtnClose } from 'components/AwesomeModal/styled'
 import { IconCancel } from 'public/icons'
 import GenerateSales from 'container/Sales'
-import { AwesomeModal } from 'components/AwesomeModal'
+// import { AwesomeModal } from 'components/AwesomeModal'
 import ScaleModal from 'components/AwesomeModal/Scale'
 // import { Messages } from 'container/messages'
 
