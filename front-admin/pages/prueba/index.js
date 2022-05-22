62870847e69b06804a256fce1e02838e13a2014a
Jesus Juvinao <53837840+Jesus123780@users.noreply.github.com>
2022-05-22 02:35:02 -0500

    subiendo drag confi

--- front-admin/pages/prueba/index.js
+++ front-admin/pages/prueba/index.js
@@ -1,10 +1,104 @@
-import { nombre } from 'pages/formas-de-pago/[...slug]'
+import React, { useRef, useState } from 'react'
+const DragNDrop = () => {
+  // https://codesandbox.io/s/k355wo7jk3?file=/index.js
+  // https://codesandbox.io/s/react-drag-and-drop-hook-o67yn?file=/src/components/DragNDrop.tsx
+  // https://codesandbox.io/s/fake-svg-drag-hook-5q5t5?file=/src/useFakeSvgDrag.js
+  const data = [
+    { title: 'group 1', items: ['1', '2', '3', '4', '1', '2', '3', '4', '1', '2', '3', '4'] },
+    { title: 'group 2', items: ['5', '6', '7'] },
+    { title: 'group 3', items: ['88', '99'] },
+    { title: 'group 4', items: ['0'] }
+  ]
 
-export default function RegisterView() {
+  const [list, setList] = useState(data)
+  
+  const [dragging, setDragging] = useState(false)
+  const initialCoOrdiate = {
+    groupIndex: 0,
+    itemIndex: 0
+  }
+  const dragItem = useRef(initialCoOrdiate)
+  const dragNode = useRef()
+  // const handleDragStart = (e: React.DragEvent, params: CoOrd) => {
+  const handleDragStart = (e, groupIndex, itemIndex) => {
+    const params = {
+      groupIndex: groupIndex,
+      itemIndex: itemIndex
+    }
+    dragItem.current = params
+    dragNode.current = e.target
+    dragNode.current.addEventListener('dragend', handleDragEnd)
+    setTimeout(() => {
+      setDragging(true)
+    }, 0)
+  }
+  const handleDragEnd = () => {
+    let _a
+    setDragging(false);
+    (_a = dragNode.current) === null || _a === void 0 ? void 0 : _a.removeEventListener('dragend', handleDragEnd)
+    dragItem.current = initialCoOrdiate
+    dragNode.current = undefined
+
+  }
+  const handleDragEnter = (e, groupIndex, itemIndex) => {
+    const params = {
+      groupIndex: groupIndex,
+      itemIndex: itemIndex
+    }
+    const currentItem = dragItem.current
+    if (e.target !== dragNode.current) {
+      setList((oldList) => {
+        let newList = JSON.parse(JSON.stringify(oldList))
+        newList[params.groupIndex].items.splice(params.itemIndex, 0, newList[currentItem.groupIndex].items.splice(currentItem.itemIndex, 1)[0])
+        // console.log(newList)
+        dragItem.current = params
+        return newList
+      })
+    }
+  }
+  const getStyles = (groupIndex, itemIndex) => {
+    const currentItem = dragItem.current
+    if (currentItem.groupIndex === groupIndex &&
+            currentItem.itemIndex === itemIndex) {
+      return 'current box-items'
+    }
+    return 'box-items'
+  }
   return (
-    <div>
-      {nombre}
+    <div className='box-container'>
+      <div className='box-container'>
+        {list.map((grp, grpIdx) => {return (
+          <div
+            className='box-group'
+            key={grp.title}
+            onDragEnter={
+              dragging && !grp.items.length
+                ? (e) => {
+                  handleDragEnter(e, grpIdx, 0)
+                }
+                : undefined
+            }
+          >
+            <div className='group-title'>{grp.title}</div>
+            {grp.items.map((item, itemIdx) => {return (
+              <div
+                className={dragging ? getStyles(grpIdx, itemIdx) : 'box-items'}
+                draggable
+                key={item}
+                onDragEnter={
+                  dragging
+                    ? (e) => {return handleDragEnter(e, grpIdx, itemIdx)}
+                    : undefined
+                }
+                onDragStart={(e) => {return handleDragStart(e, grpIdx, itemIdx)}}
+              >
+                {item}
+              </div>
+            )})}
+          </div>
+        )})}
+      </div>
     </div>
-
   )
 }
+export default DragNDrop
