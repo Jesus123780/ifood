496221e784dec5ae09f8c3d69986265e8339b516
Jesus Juvinaodfsd <juvinaojesusd@gmail.com>
2022-07-13 03:00:03 -0500

    feat creando hook de empleados

--- front-admin-store/pages/api/lib/resolvers/Store/employee.js
+++ front-admin-store/pages/api/lib/resolvers/Store/employee.js
@@ -1,16 +1,25 @@
-import employees from '../../models/Store/employees'
-import { deCode } from '../../utils/util'
+import employeesModel from '../../models/Store/employees'
+import { deCode, getAttributes } from '../../utils/util'
 
 export const createEmployee = async (_, { input }, ctx) => {
   try {
-    await employees.create({ ...input, idStore: deCode('MjcyMDg4ODE0ODUxNTE2NDUw' || ctx.restaurant) })
+    await employeesModel.create({ ...input, idStore: deCode(ctx.restaurant) })
     return {
       success: true,
       message: 'creada'
     }
   } catch (error) {
+    console.log("🚀 ~ file: employee.js ~ line 12 ~ createEmployee ~ error", error)
     return { success: false, message: error }
   }
+}
+
+export const employees = async (_, args, ctx, info) => {
+  const attributes = getAttributes(employeesModel, info)
+  return await employeesModel.findAll({
+    attributes,
+    where: { idStore: deCode('MjcyMDg4ODE0ODUxNTE2NDUw' || ctx.restaurant) }
+  })
 }
 
 
@@ -18,6 +27,7 @@ export default {
   TYPES: {
   },
   QUERIES: {
+    employees
   },
   MUTATIONS: {
     createEmployee
