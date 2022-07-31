98532a33727bbbd15ab0a18962117468d7054ac9
Jesus Juvinaodfsd <juvinaojesusd@gmail.com>
2022-07-31 03:46:05 -0500

    feat: disable contexmenu

--- front-admin-store/pages/_app.js
+++ front-admin-store/pages/_app.js
@@ -48,6 +48,29 @@ export default function App({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false)
   useEffect(() => {
     setShowChild(true)
+    document.addEventListener('contextmenu', event => {return event.preventDefault()})
+ 
+    document.onkeydown = function (e) {
+ 
+      // disable F12 key
+      if(e.keyCode == 123) {
+        return false}
+ 
+      // disable I key
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+        return false
+      }
+ 
+      // disable J key
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false
+      }
+ 
+      // disable U key
+      if(e.ctrlKey && e.keyCode == 85) {
+        return false
+      }
+    }
   }, [showChild])
 
   // useEffect(() => {
